24b5edc18cd5c76f52e7aae2ee49bc7f95cb6412
kei95 <kei0613shine.star@gmail.com>
2022-02-10 20:30:53 -0800

    add 215.kth-largest-element-in-an-array and fix typo in folder

---
+++ questions/priorityQueue/215.kth-largest-element-in-an-array.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode id=215 lang=typescript
+ *
+ * [215] Kth Largest Element in an Array
+ */
+
+import {
+  MaxPriorityQueue,
+  PriorityQueueItem,
+} from "@datastructures-js/priority-queue";
+
+/**
+ * `Priority queue`
+ *
+ * Prepare max priority queue and store all values in the `nums`.
+ * Then dequeue the item k - 1 times and eventually the top in the queue is the answer(kth largest item).
+ */
+function findKthLargest(nums: number[], k: number): number {
+  const maxPriorQueue = new MaxPriorityQueue<number>();
+
+  // iterate the array and enqueue them
+  for (const num of nums) {
+    maxPriorQueue.enqueue(num, num);
+  }
+
+  // dequeue items k - 1 times
+  while (k > 1) {
+    maxPriorQueue.dequeue();
+    k--;
+  }
+
+  // eventually, the item at front is the answer
+  const resFront = maxPriorQueue.front() as PriorityQueueItem<number>;
+  return resFront.element;
+}
+
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
