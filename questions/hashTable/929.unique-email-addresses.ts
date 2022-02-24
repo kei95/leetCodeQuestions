bcb48f0195b4b0430dbbf612bd4da26d3e5affc3
kei95 <kei0613shine.star@gmail.com>
2022-02-23 17:52:28 -0800

    update 929.unique-email-addresses.ts

--- questions/hashTable/929.unique-email-addresses.ts
+++ questions/hashTable/929.unique-email-addresses.ts
@@ -12,10 +12,10 @@
  * And remove "." and any characters beyond "+".
  * After going through all items, merge divided string and add it to table.
  *
- * time: O(n)
- * space: O(n)
+ * * time: O(n)
+ * * space: O(n)
  */
-function numUniqueEmails(emails: string[]): number {
+export function numUniqueEmails(emails: string[]): number {
   const table = new Set();
 
   for (const email of emails) {
@@ -24,8 +24,8 @@ function numUniqueEmails(emails: string[]): number {
     emailArr[0] = emailArr[0].split("+")[0];
     emailArr[0] = emailArr[0].replace(/\./g, "");
 
-    // split the email in first half and second half
-    table.add(emailArr.join("@"));
+    // Merge address and domain
+    table.add(`${emailArr[0]}@${emailArr[1]}`);
   }
 
   return table.size;
