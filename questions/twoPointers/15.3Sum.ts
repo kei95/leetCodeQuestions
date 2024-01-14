c8b537ceae7f273f255ba2fcc44af07892ab9a4e
kei95 <kei0613shine.star@gmail.com>
2024-01-14 00:54:36 -0800

    Add 3Sum

---
+++ questions/twoPointers/15.3Sum.ts
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/3sum
+
+function threeSum(nums: number[]): number[][] {
+  nums.sort((a, b) => a - b);
+  const res: number[][] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let remaining = 0 + nums[i];
+    let headIndex = i + 1;
+    let tailIndex = nums.length - 1;
+
+    // when i and headIndex is the same, home headIndex to last of it
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      headIndex++;
+      continue;
+    }
+
+    while (headIndex < tailIndex) {
+      const sum = nums[headIndex] + nums[tailIndex] + remaining;
+      // skip duplicate nums in tail
+      if (nums[tailIndex] === nums[tailIndex + 1]) {
+        tailIndex--;
+        continue;
+      }
+
+      if (sum === 0) {
+        res.push([nums[i], nums[headIndex], nums[tailIndex]]);
+        headIndex++;
+        tailIndex--;
+        continue; // do not stop until you try all the case with base i
+      }
+
+      if (sum > 0) {
+        tailIndex--;
+        continue;
+      }
+
+      if (sum < 0) {
+        headIndex++;
+        continue;
+      }
+    }
+  }
+
+  return res;
+}
+
+// time O(n^2)
+// space O(n)
