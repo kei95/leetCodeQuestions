7498ee8933d8d09de49d23427f685510c920487e
kei95 <kei0613shine.star@gmail.com>
2024-01-15 21:56:40 -0800

    Add comment on misterious regex

--- questions/twoPointers/125.valid-palindrome.ts
+++ questions/twoPointers/125.valid-palindrome.ts
@@ -1,6 +1,7 @@
 // https://leetcode.com/problems/valid-palindrome
 
 function isPalindrome(s: string): boolean {
+  // Get rid of all non-alphanumeric numbers
   const alphanumericChars = s.replace(/[^0-9a-z]/gi, "").toLowerCase();
   let headIndex = 0;
   let tailIndex = alphanumericChars.length - 1;
