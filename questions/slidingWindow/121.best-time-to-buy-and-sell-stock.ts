dd27a48bfa991d09948b80fb9c16ea8f04dacd71
kei95 <kei0613shine.star@gmail.com>
2024-01-20 13:08:56 -0800

    Add sliding window best time to buy and sell stock

---
+++ questions/slidingWindow/121.best-time-to-buy-and-sell-stock.ts
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+
+function maxProfit(prices: number[]): number {
+  let answer = 0;
+  let headIndex = 0;
+  let tailIndex = headIndex + 1;
+
+  while (tailIndex < prices.length) {
+    const profit = prices[tailIndex] - prices[headIndex];
+
+    if (profit < 0) {
+      headIndex = tailIndex;
+      tailIndex = headIndex + 1;
+      continue;
+    }
+
+    if (profit > answer) {
+      answer = profit;
+    }
+
+    tailIndex++;
+  }
+
+  return answer;
+}
+
+// time: O(1)
+// space: O(n)
