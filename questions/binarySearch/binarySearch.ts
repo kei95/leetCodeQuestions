a2f2df81544a333b3ea3baea0a1372120b0b2124
kei95 <kei0613shine.star@gmail.com>
2024-02-13 21:42:55 -0800

    Retry Binary Search with comments

--- questions/binarySearch/binarySearch.ts
+++ questions/binarySearch/binarySearch.ts
@@ -23,5 +23,35 @@ function search(nums: number[], target: number): number {
   return -1;
 }
 
-// time: O(n)
+function binarySearch(nums: number[], target: number) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    // Get the middle part of the array to divide
+    let mid = Math.floor((right + left) / 2);
+
+    if (target === nums[mid]) return mid;
+
+    // if the middle element is smaller than the target
+    // left should be where mid + 1 so that you can shrink and get
+    // another middle position
+    if (target > nums[mid]) {
+      left = mid + 1;
+      continue;
+    }
+
+    // if the middle element is greater than the target
+    // right should be where mid - 1 so that you can shrink and get
+    // another middle position
+    if (target < nums[mid]) {
+      right = mid - 1;
+      continue;
+    }
+  }
+
+  return -1;
+}
+
+// time: O(log n)
 // space: O(1)
