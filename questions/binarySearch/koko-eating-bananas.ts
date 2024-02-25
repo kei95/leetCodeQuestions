4d919ece895227ea9131eab286a9328da5b165f9
kei95 <kei0613shine.star@gmail.com>
2024-02-25 13:14:35 -0800

    Update koko-eating-bananas

--- questions/binarySearch/koko-eating-bananas.ts
+++ questions/binarySearch/koko-eating-bananas.ts
@@ -8,23 +8,24 @@ function minEatingSpeed(piles: number[], h: number): number {
 
   // Do the binary search with division factor to be "K"
   while (left <= right) {
-    let k = Math.floor((left + right) / 2);
-    let hours = 0;
+    let mid = Math.floor((left + right) / 2); // wheres k in description
+    let hoursSpent = 0;
 
     // Sum up "hours" with "Math.ceil" (round up) to determine whether if it satisfies the base requirement
-    for (const p of piles) {
-      hours += Math.ceil(p / k);
+    for (const pile of piles) {
+      hoursSpent += Math.ceil(pile / mid);
     }
 
     // If the hours is smaller than "h", we might need to update res
-    if (hours <= h) {
-      res = Math.min(res, k);
-      right = k - 1;
-    } else {
-      // skipping check for this case as it's greater than the current response
-      // since this case indicates it didn't satisfy base requirement (hours <= h)
-      left = k + 1;
+    if (hoursSpent <= h) {
+      if (res > mid) res = mid;
+      right = mid - 1;
+      continue;
     }
+
+    // skipping check for this case as it's greater than the current response
+    // since this case indicates it didn't satisfy base requirement (hours <= h)
+    left = mid + 1;
   }
 
   return res;
