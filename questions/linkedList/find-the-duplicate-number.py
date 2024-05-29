417b457925349ac4f64506563f5c7d71b551f082
kei95 <kei0613shine.star@gmail.com>
2024-05-28 23:21:06 -0700

    Add find the duplicate number

---
+++ questions/linkedList/find-the-duplicate-number.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/find-the-duplicate-number/
+
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
+            
+# Time: O(n)
+# Space: O(1)
