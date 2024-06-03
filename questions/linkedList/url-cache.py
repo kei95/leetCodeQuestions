34e3cef4c25d4e02e1840e6233ad3f9f1fb120d1
kei95 <kei0613shine.star@gmail.com>
2024-06-02 22:00:45 -0700

    Add comments on LRU cache

--- questions/linkedList/url-cache.py
+++ questions/linkedList/url-cache.py
@@ -9,21 +9,20 @@ class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {} # Map to store nodes
-        # left = LRU, right = most recently used
-        self.left, self.right = Node(0,0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
+        self.cache = {} # cache to keep track on what exists
+        self.leftNode, self.rightNode = Node(0, 0), Node(0, 0) # dummy nodes to get oldest and newest data
+        self.leftNode.next, self.rightNode.prev = self.rightNode, self.leftNode
 
-    # remove node from list
+    # Remove a given node from internal linked list
     def remove(self, node):
         prev, next = node.prev, node.next
         prev.next, next.prev = next, prev
 
-    # insert at right
+    # Insert a given node to the right most position of the linked list
     def insert(self, node):
-        prev, next = self.right.prev, self.right
-        prev.next = next.prev = node
+        prev, next = self.rightNode.prev, self.rightNode
         node.next, node.prev = next, prev
+        prev.next = next.prev = node
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -31,17 +30,19 @@ class LRUCache:
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-
+        
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
 
+        # When the length of cache exceeds capacity, remove the least used data
         if len(self.cache) > self.capacity:
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
+            lruNode = self.leftNode.next
+            self.remove(lruNode)
+            del self.cache[lruNode.key]
+        
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
