ad69979cd11d8e959de28d00e24fd60ff9ae2a42
kei95 <kei0613shine.star@gmail.com>
2024-06-09 21:03:58 -0700

    Add inline explanations to reverse nodes in k group

--- questions/linkedList/reverse-nodes-in-k-group.py
+++ questions/linkedList/reverse-nodes-in-k-group.py
@@ -26,9 +26,9 @@ class Solution:
                 prev = curr
                 curr = tmp
 
-            tmp = groupPrev.next
-            groupPrev.next = kth
-            groupPrev = tmp
+            tmp = groupPrev.next # preserve the next "groupPrev"
+            groupPrev.next = kth # update CURRENT groupPrev's next position to be the beginning
+            groupPrev = tmp # Update it to next groupPrev where the edge of the current scope
         return dummy.next
 
     def getKth(self, curr, k):
@@ -36,3 +36,6 @@ class Solution:
             curr = curr.next
             k -= 1
         return curr
+    
+# time: O(n)
+# space: O(1)
