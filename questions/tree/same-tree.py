ef4af7aeeece4042f46f7d05ca2d82acd555485b
kei95 <kei0613shine.star@gmail.com>
2024-06-17 21:53:55 -0700

    Add same tree

---
+++ questions/tree/same-tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
+
+        return (self.isSameTree(q.left, p.left) and self.isSameTree(q.right, p.right))
